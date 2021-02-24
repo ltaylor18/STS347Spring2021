1394fb55bead5019cd24c52d505e5acdd478a90e
ltaylor18 <ltaylor18@elon.edu>
2021-02-24 11:02:00 -0500

    2021/02/24

--- Lesson 4 - 2020_02_22.R
+++ Lesson 4 - 2020_02_22.R
@@ -1,4 +1,8 @@
-#Lesson 4-1 2021-02-22
+##############
+# Lesson 4-1 #
+##############
+# 2021-02-22 #
+##############
 
 library(ggplot2)
 head(mtcars)
@@ -31,3 +35,42 @@ grid.arrange(g2, arrangeGrob(g3, g4, ncol=2), nrow = 2)
 grid.arrange(g1, g2, g3, nrow = 3)
 grid.arrange(g2, arrangeGrob(g3, g4, ncol=2), nrow = 1)
 grid.arrange(g2, arrangeGrob(g3, g4, nrow=2), nrow = 1)
+
+##############
+# 2021-02-24 #
+##############
+
+# Link for Google Slides: https://docs.google.com/presentation/d/1qKlbnwg1oCRTJdV59xPMULGa-tveLzSwv2PmCBX1dLk/edit?usp=sharing
+
+library(ggplot2)
+p <- ggplot(Orange,aes(x=age,y=circumference))
+p
+p + geom_point()
+
+#2
+p <- ggplot(Orange,aes(x=age,y=circumference,color="purple"))
+p + geom_point()
+
+#3
+p <- ggplot(Orange,aes(x=age,y=circumference))
+p + geom_point(color="green")
+
+#4
+p <- ggplot(Orange,aes(x=age,y=circumference,color=Tree))
+p + geom_point()
+
+
+
+# Following up on forum questions related to attach
+names(ChickWeight)
+Chick
+attach(ChickWeight)
+Chick
+detach(ChickWeight)
+Chick
+
+# Also, I am curious about the 157 and 164 for number of sevens
+# and would love to see your different solutions posted in the forums.
+
+
+
