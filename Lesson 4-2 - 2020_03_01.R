bc17a5c6f58b29dfc9c01f7c2b6610ce98518c72
ltaylor18 <ltaylor18@elon.edu>
2021-03-05 12:27:56 -0500

    3/5/21

--- Lesson 4-2 - 2020_03_01.R
+++ Lesson 4-2 - 2020_03_01.R
@@ -175,6 +175,7 @@ p + geom_boxplot(outlier.colour = "red", outlier.shape = 1)
 
 
 #Texas Housing One Possible Solution
+library(ggplot2)
 fig <- ggplot(txhousing,aes(x=factor(year),
                             y=sales,
                             fill=factor(year)))
@@ -194,3 +195,78 @@ fig +
 # 2021-03-05 #
 ##############
 
+# NTS: Run each layer one at a time to illustrate!
+g <- ggplot(trees,aes(x=Height,y=Girth))
+g + geom_point(size=4) + 
+  geom_vline(xintercept=64, col="red",linetype=2,size=2) + 
+  geom_hline(yintercept=15) + 
+  geom_abline(slope=0.1,intercept=5,color="purple",size=3,linetype=3) +
+  geom_segment(x=70,xend=80,y=10,yend=18,color="green") +
+  geom_text(x=66,y=18,label="My Height",color="magenta") +
+  geom_smooth(method="lm") +
+  geom_text(x=80,y=20,label="r=0.519")
+
+
+
+#Least-Squares Regression Scatterplot
+#Challenge Problem Solution
+
+myx <- c(2,2,10)
+myy <- c(2,6,10)
+mygroup <- c("A","B","C")
+
+mydata <- data.frame(myx,myy,mygroup)
+
+myfig <- ggplot(mydata,aes(x=myx,y=myy))
+myfig + 
+  geom_smooth(method="lm",se=FALSE,color="purple",size=2) +
+  geom_segment(x=2,xend=2,y=4,yend=6,color="green",linetype=2) +
+  geom_segment(x=2,xend=2,y=4,yend=2,color="red",linetype=2) +
+  geom_point(aes(color=mygroup),size=5,show.legend=FALSE) +
+  labs(x="Explanatory Variable",
+       y="Response Variable",
+       title="A Graph Showing Residuals",
+       subtitle="The Least-Squares Regression Line") +
+  geom_text(x=3,y=3,label="Residual = (-2)*(-2)=4",color="red") + 
+  geom_text(x=3,y=5,label="Residual = (2)*(2)=4",color="green") + 
+  geom_text(x=8.5,y=10,label="Residual = 0*0 = 0",color="blue") +
+  geom_text(x=7,y=6.5,label="Sum of Squared Residuals = 8",
+            color="purple") 
+
+
+# Changing the axes:
+ggplot(mtcars,aes(x=wt,y=mpg,color=factor(am)))+
+  geom_point(alpha=0.75,shape=10)+
+  geom_smooth(se=FALSE)
+
+ggplot(mtcars,aes(x=wt,y=mpg,color=factor(am)))+
+  geom_point(alpha=0.75,shape=10)+
+  geom_smooth(se=FALSE)+
+  scale_x_continuous(breaks=c(2,3,4,5),
+                     labels=c(2000,3000,4000,5000))
+
+ggplot(mtcars,aes(x=wt,y=mpg,color=factor(am)))+
+  geom_point(alpha=0.75,shape=10)+
+  geom_smooth(se=FALSE)+
+  scale_x_continuous(breaks=c(2,3,4,5),
+                     labels=c(2000,3000,4000,5000))
+
+# Example solution:
+#Example - update the graph.
+ggplot(mtcars,aes(x=wt,y=mpg,color=factor(am)))+
+  geom_point(alpha=0.75,shape=10)+
+  geom_smooth(se=FALSE)+
+  scale_x_continuous(breaks=c(2,3,4,5),
+                     labels=c(2000,3000,4000,5000))+
+  scale_y_continuous(breaks=seq(2,36,2))
+
+
+# Example with scale_x_discrete:
+ggplot(mtcars,aes(y=hp,x=factor(am)))+
+  geom_boxplot()
+
+ggplot(mtcars,aes(y=hp,x=factor(am)))+
+  geom_boxplot()+
+  scale_x_discrete(name="Transmission",
+                   breaks=c(0,1),
+                   labels=c("0"="Auto","1"="Manual"))
