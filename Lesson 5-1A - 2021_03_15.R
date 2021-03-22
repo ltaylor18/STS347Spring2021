ce58633e5706ee600fbbb4b24ce60e73338336ae
ltaylor18 <ltaylor18@elon.edu>
2021-03-22 13:14:58 -0400

    3/22/21

--- Lesson 5-1A - 2021_03_15.R
+++ Lesson 5-1A - 2021_03_15.R
@@ -77,3 +77,85 @@ pnorm(1.96,lower.tail=FALSE)
 mynewnorm <- rnorm(100000,0,1)
 sum(mynewnorm > 1.96)/100000
 sum(mynewnorm >= 1.96)/100000
+
+
+###########
+# 3/22/21 #
+###########
+
+
+# Proof of t-distribution
+
+# 1. Since we can't generate an infinite number of random samples,
+# Let's settle for generating 50,000 samples of size 15 from a 
+# Normal Distribution matching the population our soda sample
+# came from (I'll use the mean=12 and sd=0.1),
+
+mysamples <- replicate(50000,rnorm(15,12,0.1))
+
+# Each column of mysamples represents one replication of my
+# soda experiment of observing 15 cans.
+dim(mysamples)
+mysamples[,1] #check out the first sample in the first column
+
+# 2. Calculate the t-test statistic for each column of my data.
+
+# Find the mean and standard deviation of each sample of 15 observations
+mymeans <- apply(mysamples,2,mean)
+length(mymeans) #we get 50,000 x-bars!
+mysds <- apply(mysamples,2,sd)
+# Calculate t for each sample of 15 observations
+myteststats <- (mymeans - 12)/(mysds/sqrt(15))
+length(myteststats)
+
+
+# 3. Observe the distribution of my calculated test statistics
+
+library(ggplot2)
+ggplot(NULL,aes(x=myteststats)) +
+  geom_histogram(aes(y=..density..), color="white")
+
+# 4. Super-impose the t-14 distribution on the histogram 
+# of test statistics and generate a QQ-Plot to compare
+# the test statistics to the Normal distribution.
+
+myx <- seq(-4,4,.01)
+myy <- dt(myx,14)
+ggplot(NULL, aes(x=myteststats)) +
+  geom_histogram(aes(y=..density..), color="white") +
+  geom_line(aes(x=myx,y=myy),color="red") +
+  labs(title="50,000 t Test Statistics Compared to t-14 curve")
+
+ggplot(NULL,aes(sample=myteststats)) +
+  stat_qq() +
+  geom_qq_line(distribution=stats::qnorm)
+
+# 5. Until 1901, statisticians used the Normal Distribution
+# to model the sampling distribution of this test statistic.
+# How "bad" was that? Well, here' I've added the Normal
+# Distribution's probability density function in blue. 
+# What do you think?
+
+myx <- seq(-4,4,.01)
+myy <- dt(myx,14)
+mynormy <- dnorm(myx)
+ggplot(NULL, aes(x=myteststats)) +
+  geom_histogram(aes(y=..density..), color="white") +
+  geom_line(aes(x=myx,y=myy),color="red") + 
+  geom_line(aes(x=myx,y=mynormy),color="blue",linetype=2)
+
+
+#Is it a big difference between Z and T?
+
+pnorm(1.96, lower.tail=FALSE)
+
+pt(1.96,4,lower.tail=FALSE)
+
+pt(1.96,9,lower.tail=FALSE)
+
+pt(1.96,19,lower.tail=FALSE)
+
+pt(1.96,29,lower.tail=FALSE)
+
+
+
---
+++ Notes 5-1B - 2021_03_22.R
@@ -0,0 +1,47 @@
+## Lesson 5-1B##
+
+###########
+# 3/22/21 #
+###########
+
+#Notes 5-1B
+
+pt(1.28, df=24, lower.tail = TRUE)
+pt(1.28,24,lower.tail=TRUE)
+?pt
+
+#Repeating probability from example through simulation
+
+sum(myteststats < -0.90)/50000
+pt(-0.90,14)
+
+pt(1.28, 24)
+
+
+pt(1.28, 24, lower.tail=FALSE)
+1-pt(1.28,24)
+
+#Example Table
+#P(T12 < 2)
+pt(2,12)
+
+#P(T5 > - 0.5)
+pt(-.5,5,lower.tail=FALSE)
+1-pt(-.5,5)
+
+#P(T99 > 1.28)
+pt(1.28,99,lower.tail=FALSE)
+1-pt(1.28,99)
+
+
+
+set.seed(2320)
+samp <- rnorm(1000,mean=250,sd=25)
+
+xbar <- mean(samp)
+s <- sd(samp)
+
+t <- (xbar-250)/(s/sqrt(1000))
+
+pt(t,999) #Correct
+pnorm(t)  #Incorrect
